cad635c6c001d04b4142e02e94b6bfafaf588f40
efrain <efrain.areleon.gmail.com>
2024-11-04 20:53:03 -0600

    Integración de proyecto API a  repositorio

---
+++ GestorEmpleados/Models/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiWebAPI.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Producto { get; set; }
+        public int Canitdad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Total { get; set; }
+
+        public decimal Subtotal ;
+        public string Cliente { get; set; }
+    }
+}
